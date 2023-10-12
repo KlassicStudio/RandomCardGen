c91ccb9f8f78c0f4f31b247618f3427af6c2708a
Martin Nusspaumer <142355408+KlassicStudio@users.noreply.github.com>
2023-10-12 16:45:53 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+let icons = ['♠', '♣', '♥', '♦' ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let randomCards = cards[Math.floor(Math.random() * cards.length)];
+let randomIcons = icons[Math.floor(Math.random() * icons.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+    document.querySelector('.top').innerHTML = randomIcons;
+    document.querySelector('.bottom').innerHTML = randomIcons;
+    document.querySelector('.middle').innerHTML = randomCards;
+
+    if (randomIcons === '♥') {
+        document.getElementById("icon-top").innerHTML=`<span style="color:red">♥</span>`;
+    }
+
+    if (randomIcons === '♥') {
+        document.getElementById("icon-bottom").innerHTML=`<span style="color:red">♥</span>`;
+    }
+
+    if (randomIcons === '♦') {
+        document.getElementById("icon-top").innerHTML=`<span style="color:red">♦</span>`;
+    }
+
+    if (randomIcons === '♦') {
+        document.getElementById("icon-bottom").innerHTML=`<span style="color:red">♦</span>`;
+    }
+
 };
+
+
+
+function reloadPage() {
+    location.reload();
+  }
+
+console.log(randomCards + randomIcons);
+
