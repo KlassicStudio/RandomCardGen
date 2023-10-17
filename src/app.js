f41bb0518c0c13512bef83a73076fb5e7864643d
Martin Nusspaumer <142355408+KlassicStudio@users.noreply.github.com>
2023-10-17 00:12:24 +0000

    changes to run on npm

--- src/app.js
+++ src/app.js
@@ -1,45 +1,57 @@
-let cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-let icons = ['♠', '♣', '♥', '♦' ];
+import "./style.css";
+import "./assets/img/jack.jpg";
+import "./assets/img/king.jpg";
+import "./assets/img/queen.jpg";
 
+let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let icons = ["♠", "♣", "♥", "♦"];
 
+window.onload = function() {
+  document.getElementById("dealer").addEventListener("click", dealer);
+};
 function dealer() {
+  let randomCards = cards[Math.floor(Math.random() * cards.length)];
+  let randomIcons = icons[Math.floor(Math.random() * icons.length)];
 
-    let randomCards = cards[Math.floor(Math.random() * cards.length)];
-    let randomIcons = icons[Math.floor(Math.random() * icons.length)];
+  document.querySelector(".top").innerHTML = randomIcons;
+  document.querySelector(".bottom").innerHTML = randomIcons;
+  document.querySelector(".middle").innerHTML = randomCards;
 
+  if (randomIcons === "♥") {
+    document.getElementById(
+      "icon-top"
+    ).innerHTML = `<span style="color:red">♥</span>`;
+    document.getElementById(
+      "icon-bottom"
+    ).innerHTML = `<span style="color:red">♥</span>`;
+    document.getElementById("value").style.color = "red";
+  } else if (randomIcons === "♦") {
+    document.getElementById(
+      "icon-top"
+    ).innerHTML = `<span style="color:red">♦</span>`;
+    document.getElementById(
+      "icon-bottom"
+    ).innerHTML = `<span style="color:red">♦</span>`;
+    document.getElementById("value").style.color = "red";
+  } else {
+    document.getElementById("value").style.color = "black";
+  }
 
-        document.querySelector('.top').innerHTML = randomIcons;
-        document.querySelector('.bottom').innerHTML = randomIcons;
-        document.querySelector('.middle').innerHTML = randomCards;
+  if (randomCards === "K") {
+    document.getElementById(
+      "value"
+    ).innerHTML = `<img src="./king.jpg" alt=""></img>`;
+  }
 
-             if (randomIcons === '♥') {
-               document.getElementById("icon-top").innerHTML=`<span style="color:red">♥</span>`;
-               document.getElementById("icon-bottom").innerHTML=`<span style="color:red">♥</span>`;
-               document.getElementById("value").style.color = "red";
-             
-            } else if (randomIcons === '♦') {
-               document.getElementById("icon-top").innerHTML=`<span style="color:red">♦</span>`;
-               document.getElementById("icon-bottom").innerHTML=`<span style="color:red">♦</span>`;
-               document.getElementById("value").style.color = "red";
-            } else {
-              document.getElementById("value").style.color = "black";
-            }
-    
-             if (randomCards==='K') {
-               document.getElementById("value").innerHTML=`<img src="assets/img/king.jpg" alt=""></img>`;
-              
-            }
+  if (randomCards === "Q") {
+    document.getElementById(
+      "value"
+    ).innerHTML = `<img src="./queen.jpg" alt=""></img>`;
+  }
 
-             if (randomCards==='Q') {
-               document.getElementById("value").innerHTML=`<img src="assets/img/queen.jpg" alt=""></img>`;
-              
-            }
-
-             if (randomCards==='J') {
-              document.getElementById("value").innerHTML=`<img src="assets/img/jack.jpg" alt=""></img>`;
-         
-            }
-
-
+  if (randomCards === "J") {
+    document.getElementById(
+      "value"
+    ).innerHTML = `<img src="./jack.jpg" alt=""></img>`;
+  }
 }
-
